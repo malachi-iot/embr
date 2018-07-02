7326968d8d419d00431c794322f422ce7df6d35e
Malachi Burke <malachi.burke@gmail.com>
2018-07-02 14:08:26 -0700

    Boilerplate

---
+++ test/catch/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by malachi on 10/20/17.
+//
+
+#ifdef _MSC_BUILD
+#define CATCH_CONFIG_RUNNER
+#else
+#define CATCH_CONFIG_MAIN
+#endif
+
+#include <catch.hpp>
+
+TEST_CASE("main")
+{
+}
