c5fdbc992d943f60072711d99db16c80d3b7a18e
Malachi Burke <malachi.burke@gmail.com>
2019-10-29 19:24:40 -0700

    Prepping to set up WiFi
    Still not fully active, and unexpected event loop error appearing also

--- test/rtos/esp-idf/test/main/main.cpp
+++ test/rtos/esp-idf/test/main/main.cpp
@@ -4,11 +4,55 @@
 
 #include <embr/streambuf.h>
 
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
 static void print_banner(const char* text);
 
+// largely copy/paste from
+// https://github.com/espressif/esp-idf/blob/v3.3/examples/wifi/getting_started/station/main/station_example_main.c
+void wifi_init_sta()
+{
+    static const char *TAG = "wifi_init_sta";
+
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    wifi_config_t wifi_config;
+    
+    //wifi_config.sta.ssid = CONFIG_WIFI_SSID;
+    //wifi_config.sta.password = CONFIG_WIFI_PASSWORD;
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "finished.");
+    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
+             CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
+}
+
 extern "C" void app_main()
 {
+    static const char *TAG = "app_main";
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+
+    // --------
+
     print_banner("Running all the registered tests");
     UNITY_BEGIN();
     unity_run_all_tests();
