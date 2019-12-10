a45ab5be6d1d0ca882f73743b24ed8d15a0bb23d
Malachi Burke <malachi.burke@gmail.com>
2019-12-10 08:41:26 -0800

    Fiddling

--- src/embr/platform/lwip/udp.h
+++ src/embr/platform/lwip/udp.h
@@ -16,21 +16,36 @@ struct Pcb
 {
     typedef struct udp_pcb* pcb_pointer;
     typedef struct pbuf* pbuf_pointer;
+    typedef const ip_addr_t* addr_pointer;
 
 private:
     pcb_pointer pcb;
 public:
     Pcb(pcb_pointer pcb) : pcb(pcb) {}
 
-    void sendto(pbuf_pointer pbuf, 
-        const ip_addr_t* addr,
+    err_t send(pbuf_pointer pbuf, 
+        addr_pointer addr,
         uint16_t port)
     {
-        udp_sendto(pcb, 
+        return udp_sendto(pcb, 
             pbuf, 
             addr, 
             port);
     }
+
+    void recv(udp_recv_fn recv_f, void* recv_arg)
+    {
+        udp_recv(pcb, recv_f, recv_arg);
+    }
+
+    // connected only receives data from the connected address,
+    // unconnected receive from any address
+    err_t connect(ip_addr_t* ipaddr, u16_t port)
+    {
+        return udp_connect(pcb, ipaddr, port);
+    }
+
+    void disconnect() { udp_disconnect(pcb); }
 };
 
 }}
