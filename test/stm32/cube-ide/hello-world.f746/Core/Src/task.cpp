2d289058093e668a6555bcd36456647f63b1ec1e
Malachi Burke <malachi.burke@gmail.com>
2019-12-12 08:53:55 -0800

    Reimplenting lost STM32 minimal support code
    Compiles and runs nicely !

--- test/stm32/cube-ide/hello-world.f746/Core/Src/task.cpp
+++ test/stm32/cube-ide/hello-world.f746/Core/Src/task.cpp
@@ -14,29 +14,16 @@
 #include "cmsis_os.h"
 #include "usb_device.h"
 
-// wouldn't it be nice if...
-// http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0491i/BABJFEFG.html
-//#if defined(__TARGET_CPU_CORTEX_M7) || defined(__TARGET_CPU_CORTEX_M4) ||
-#if defined(__ARM_ARCH_7EM__)
-// Does get here, that's cool.  Not referenced on above link...
-//#error Would love to see this error
-#endif
-
 
 // Just to test we're in fact getting *something* -- but looking good
 #include <estd/string.h>
-// OK, these *were* working but symlinking lied to me and I accidentally deleted the
-// support code.  The changes are:
-// 1. add a platform-detector in estd
-// 2. stringconvert.cpp: notice it's ARM/STM32 platform to enable itoa and friends
-// 3. stringconvert.cpp: notice it's STM32 and disable the sprintf float code
 
 extern "C" void hello_task()
 {
     uint8_t buffer[] = "Hello, World!\r\n";
 
-    //estd::layer2::const_string s = "Hi2u:"; // the ptr-to-null-terminated variety
-    //uint8_t* data = reinterpret_cast<uint8_t*>(const_cast<char*>(s.data()));
+    estd::layer2::const_string s = "Hi2u:"; // the ptr-to-null-terminated variety
+    uint8_t* data = reinterpret_cast<uint8_t*>(const_cast<char*>(s.data()));
 
     /* Infinite loop */
     for(;;)
@@ -44,8 +31,8 @@ extern "C" void hello_task()
         // Verified works after a few seconds of bringup, miniterm.py in debian
         // can see our Hello, World! though there's some junk inbetween
 
-        //CDC_Transmit_FS(data, s.length());
-        //osDelay(500);
+        CDC_Transmit_FS(data, s.length());
+        osDelay(500);
         CDC_Transmit_FS(buffer, sizeof(buffer) - 1);
 
         osDelay(3000);
