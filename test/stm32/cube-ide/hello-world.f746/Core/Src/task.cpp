09b79863e7f4444a05703c15e0269aa73ed5d477
Malachi Burke <malachi.burke@gmail.com>
2019-12-11 20:34:10 -0800

    Was working towards estd compat, but no
    Symlinking didnt work as expected, and I goofed myself up and
    deleted the code

--- test/stm32/cube-ide/hello-world.f746/Core/Src/task.cpp
+++ test/stm32/cube-ide/hello-world.f746/Core/Src/task.cpp
@@ -14,16 +14,30 @@
 #include "cmsis_os.h"
 #include "usb_device.h"
 
+// Just to test we're in fact getting *something* -- but looking good
+//#include <estd/string.h>
+// OK, these *were* working but symlinking lied to me and I accidentally deleted the
+// support code.  The changes are:
+// 1. add a platform-detector in estd
+// 2. stringconvert.cpp: notice it's ARM/STM32 platform to enable itoa and friends
+// 3. stringconvert.cpp: notice it's STM32 and disable the sprintf float code
+
 extern "C" void hello_task()
 {
     uint8_t buffer[] = "Hello, World!\r\n";
+
+    //estd::layer2::const_string s = "Hi2u:"; // the ptr-to-null-terminated variety
+    //uint8_t* data = reinterpret_cast<uint8_t*>(const_cast<char*>(s.data()));
 
     /* Infinite loop */
     for(;;)
     {
         // Verified works after a few seconds of bringup, miniterm.py in debian
         // can see our Hello, World! though there's some junk inbetween
-        CDC_Transmit_FS(buffer, sizeof(buffer));
+
+        //CDC_Transmit_FS(data, s.length());
+        //osDelay(500);
+        CDC_Transmit_FS(buffer, sizeof(buffer) - 1);
 
         osDelay(3000);
     }
