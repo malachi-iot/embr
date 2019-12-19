cc1b19c3d07dfa740b8bfad3ca22850f53b8edc5
Malachi Burke <malachi.burke@gmail.com>
2019-12-19 06:51:13 -0800

    Intermediate untested / uncompiled change due to emergency

--- src/embr/platform/lwip/streambuf.h
+++ src/embr/platform/lwip/streambuf.h
@@ -10,11 +10,13 @@ namespace embr { namespace lwip {
 // out_basic_pbuf_streambuf<char> and out_pbuf_streambuf (no char)
 #ifdef FEATURE_CPP_ALIASTEMPLATE
 template <class CharT, class CharTraits = std::char_traits<CharT> >
-using out_pbuf_streambuf = embr::mem::out_netbuf_streambuf<CharT, PbufNetbuf, CharTraits>;
+using basic_opbuf_streambuf = embr::mem::out_netbuf_streambuf<CharT, PbufNetbuf, CharTraits>;
 
 template <class CharT, class CharTraits = std::char_traits<CharT> >
-using in_pbuf_streambuf = embr::mem::in_netbuf_streambuf<CharT, PbufNetbuf, CharTraits>;
+using basic_ipbuf_streambuf = embr::mem::in_netbuf_streambuf<CharT, PbufNetbuf, CharTraits>;
 #endif
 
+typedef embr::mem::out_netbuf_streambuf<char, PbufNetbuf> opbuf_streambuf;
+typedef embr::mem::in_netbuf_streambuf<char, PbufNetbuf> ipbuf_streambuf;
 
 }}
