9c97bf4b6df0a64485705b49df9e2801ed914315
Malachi Burke <malachi.burke@gmail.com>
2019-12-25 12:38:22 -0800

    More experimental buildout

--- src/embr/platform/freertos/exp/transport-retry.h
+++ src/embr/platform/freertos/exp/transport-retry.h
@@ -7,10 +7,12 @@
  *  1) sessionless, no inbuilt sequence mechanism
  *  2) non blocking/async
  * 
- *  Also depends heavily on estd::streambuf
+ *  Also depends heavily on estd::streambuf, and depends it having
+ *  a netbuf-style 'reset' capability (perhaps done through pubseekoff)
  */
 
 #include <estd/internal/platform.h>
+#include <estd/forward_list.h>
 
 #ifdef ESP_PLATFORM
 #include <freertos/FreeRTOS.h>
@@ -24,21 +26,37 @@ namespace embr { namespace experimental {
 // We expect TTransport to be conforming to something like ../lwip/transport.h
 //    an important component of that is that it accepts a TStreambuf for input
 // We expect TRetryPolicyImpl to help us choose how long retry delays are as well
-//    as how many times to retry
+//    as how many times to retry, and how to compare our streambuf to incoming
+//    streambuf to make sure it's an app-specific match (e.g. matching on CoAP MID)
 template <class TTransport, class TRetryPolicyImpl>
 struct RetryManager
 {
     TRetryPolicyImpl policy_impl;
 
+    typedef typename TRetryPolicyImpl::key_type key_type;
     typedef typename TTransport::endpoint_type endpoint_type;
     typedef typename TTransport::ostreambuf_type ostreambuf_type;
+    typedef typename TTransport::istreambuf_type istreambuf_type;
+    typedef typename TRetryPolicyImpl::item_policy_impl_type item_policy_impl_type;
 
-    struct QueuedItem
+    key_type extract_key(istreambuf_type& streambuf)
     {
+        return policy_impl.extract_key(streambuf);
+    }
+
+    struct QueuedItem : 
+        estd::experimental::forward_node_base_base<QueuedItem*>,
+        item_policy_impl_type
+    {
+        // endpoint that we want to send to.  For ipv4 this is IP address and port
         endpoint_type endpoint;
+        // streambuf to be (repeatedly) send to aforementioned endpoint
         ostreambuf_type& streambuf;
         unsigned retry_count;
+        // NOTE: may or may not want to cache this here, but probably yes
+        key_type key;
 
+        // IDEA: may be convenient to pass in key from outside app
         QueuedItem(const endpoint_type& endpoint, ostreambuf_type& streambuf) :
             endpoint(endpoint),
             streambuf(streambuf),
@@ -46,12 +64,27 @@ struct RetryManager
         {
 
         }
+
+        bool match(const endpoint_type& endpoint, const key_type& key)
+        {
+            return false;
+        }
     };
 
+    estd::intrusive_forward_list<QueuedItem> items;
 
-    void add(const endpoint_type& endpoint, ostreambuf_type& streambuf)
+    void send(const endpoint_type& to, ostreambuf_type& streambuf)
     {
 
+    }
+
+
+    // Shall need an app-specific identifier, endpoint alone is not enough
+    // (generally) to distinguish whether this is the specific item in question
+    // streambuf will need inspection (for CoAP, we'll be looking for MID)
+    void evaluate_received(const endpoint_type& from, istreambuf_type& streambuf)
+    {
+        key_type key = extract_key(streambuf);
     }
 };
 
