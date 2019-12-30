f288dd52ff9152005f27dc5e857cd92b7e9516ff
Malachi Burke <malachi.burke@gmail.com>
2019-12-30 10:18:35 -0800

    Making experimental 'transport' more netbuf-aware
    Also elevating Endpoint to a first class citizen

--- src/embr/platform/lwip/transport.h
+++ src/embr/platform/lwip/transport.h
@@ -1,3 +1,7 @@
+/**
+ * @file
+ * Higher level wrapper around pcb for UDP send operations
+ */
 #pragma once
 
 #include "udp.h"
@@ -5,22 +9,24 @@
 
 namespace embr { namespace lwip { namespace experimental {
 
+typedef const ip_addr_t* addr_pointer;
+
+struct Endpoint
+{
+    addr_pointer address;
+    uint16_t port;
+};
+
 
 struct TransportBase
 {
-    typedef const ip_addr_t* addr_pointer;
+    typedef embr::lwip::experimental::addr_pointer addr_pointer;
     typedef struct pbuf* pbuf_pointer;
     typedef opbuf_streambuf ostreambuf_type;
     typedef ipbuf_streambuf istreambuf_type;
-
-    struct Endpoint
-    {
-        addr_pointer address;
-        uint16_t port;
-    };
+    typedef ostreambuf_type::netbuf_type netbuf_type;
 
     typedef Endpoint endpoint_type;
-
 };
 
 struct TransportUdp : 
@@ -42,6 +48,13 @@ struct TransportUdp :
             endpoint.port);
     }
 #endif
+
+    void send(netbuf_type& netbuf, const endpoint_type& endpoint)
+    {
+        pcb.send(netbuf.pbuf(),
+            endpoint.address,
+            endpoint.port);
+    }
 };
 
 }}}
