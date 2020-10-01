7f87b91a909758b3cfa863875b2f1155e00ec2c0
Malachi Burke <malachi.burke@gmail.com>
2020-10-01 09:59:00 -0700

    Beginning replacement pbuf streambuf handlers

--- src/embr/platform/lwip/streambuf.h
+++ src/embr/platform/lwip/streambuf.h
@@ -20,4 +20,34 @@ using basic_ipbuf_streambuf = embr::mem::in_netbuf_streambuf<CharT, PbufNetbuf, 
 typedef embr::mem::out_netbuf_streambuf<char, PbufNetbuf> opbuf_streambuf;
 typedef embr::mem::in_netbuf_streambuf<char, PbufNetbuf> ipbuf_streambuf;
 
+// NOTE: Not ready for prime time yet.  Will displace all my netbuf abstractions, they are finally
+// going away and replaced by more straightforward estd::streambuf capabilities
+namespace upgrading {
+
+struct pbuf_streambuf_base
+{
+
+};
+
+template <class TCharTraits>
+struct opbuf_streambuf : 
+    pbuf_streambuf_base,
+    estd::internal::out_pos_streambuf_base<TCharTraits>
+{
+    typedef estd::internal::out_pos_streambuf_base<TCharTraits> base_type;
+    typedef TCharTraits traits_type;
+};
+
+template <class TCharTraits>
+struct ipbuf_streambuf : 
+    pbuf_streambuf_base,
+    estd::internal::in_pos_streambuf_base<TCharTraits>
+{
+    typedef estd::internal::in_pos_streambuf_base<TCharTraits> base_type;
+    typedef TCharTraits traits_type;
+};
+
+
+}
+
 }}
