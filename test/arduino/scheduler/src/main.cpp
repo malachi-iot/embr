2c2a8747e6a888b5df2cae7ea5df34f92453e3ad
Malachi Burke <malachi.burke@gmail.com>
2022-04-21 01:15:17 -0700

    Boilerplate

--- test/arduino/scheduler/src/main.cpp
+++ test/arduino/scheduler/src/main.cpp
@@ -1,22 +1,25 @@
 #include <Arduino.h>
 
-//#include <algorithm>
-
 #include <estd/string.h>
 #include <estd/chrono.h>
 #include <estd/queue.h>
 #include <estd/thread.h>
 
 #include <embr/observer.h>
-//#include <embr/scheduler.h>
+#include <embr/scheduler.h>
+
+CONSTEXPR int LED_PIN = LED_BUILTIN;
 
 void setup()
 {
-
+    pinMode(LED_PIN, OUTPUT);
 }
 
 
 void loop()
 {
-
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);
 }
