b6d9ba58d49deb0ebe9467c17c7de9159e1603e5
Malachi Burke <malachi.burke@gmail.com>
2022-05-03 13:12:56 -0700

    Beefing up observer test
    Lean stack usage so far

--- test/rtos/esp-idf/observer/main/main.cpp
+++ test/rtos/esp-idf/observer/main/main.cpp
@@ -2,7 +2,49 @@
 
 #include <esp_log.h>
 
-void udp_echo_init();
+#include <embr/observer.h>
+
+struct event1 {};
+
+struct Context
+{
+    int stuff1, stuff2;
+};
+
+template <class TSubject>
+void notify_from_int(TSubject& s)
+{
+    s.notify(int{5});
+}
+
+template <class TSubject>
+void notify_from_event1(TSubject& s)
+{
+    s.notify(event1());
+}
+
+
+template <class TSubject>
+void notify_from_event1_with_context(TSubject& s)
+{
+    Context c;
+    s.notify(event1(), c);
+}
+
+struct Observer1
+{
+    static constexpr const char* TAG = "Observer1";
+
+    static void on_notify(int val)
+    {
+        ESP_LOGI(TAG, "Got int notify");
+    }
+
+    static void on_notify(event1)
+    {
+        ESP_LOGI(TAG, "Got event1 notify");
+    }
+};
 
 extern "C" void app_main()
 {
@@ -11,5 +53,11 @@ extern "C" void app_main()
     init_flash();
 
     ESP_LOGI(TAG, "Startup");
+
+    auto s = embr::layer0::subject<Observer1>();
+
+    notify_from_int(s);
+    notify_from_event1(s);
+    notify_from_event1_with_context(s);
 }
 
