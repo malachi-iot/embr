f40ce6e232b87eb5e6ac8634a2a78915abd13db0
Malachi Burke <malachi.burke@gmail.com>
2022-05-20 21:58:12 -0700

    Compiles, but not run tested

--- test/rtos/esp-idf/twai/main/twai.cpp
+++ test/rtos/esp-idf/twai/main/twai.cpp
@@ -1,3 +1,19 @@
+#include <embr/platform/esp-idf/twai.h>
+
+using namespace embr::esp_idf;
+
 void twai_init()
 {
+}
+
+void twai_loop()
+{
+    itwai_streambuf s;
+
+    int v = s.sbumpc();
+
+    otwai_streambuf s2;
+
+    s2.sputc(v);
+    s2.sputn("hi", 2);
 }
