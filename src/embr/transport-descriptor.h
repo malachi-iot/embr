356bd3b914615ab360d709435cba8f5c03a754e6
Malachi Burke <malachi.burke@gmail.com>
2022-05-22 15:19:35 -0700

    Fixing names in possibly soon to be phased out transport decriptor

--- src/embr/transport-descriptor.h
+++ src/embr/transport-descriptor.h
@@ -6,10 +6,13 @@
 namespace embr {
 
 // bundle these two together, since they are paired all over the darned place
-template <class TNetBuf, class TEndpoint>
+///
+/// \tparam TBuffer Low level buffer which transport natively uses
+/// \tparam TEndpoint Full address - e.g. for IPv4 has 4 digit addr plus port
+template <class TBuffer, class TEndpoint>
 struct TransportDescriptor
 {
-    typedef TNetBuf netbuf_type;
+    typedef TBuffer buffer_type;
     typedef TEndpoint endpoint_type;
 };
 
