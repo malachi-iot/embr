a4869c1da0ab9016adfab14a58863e9880e10237
Malachi Burke <malachi.burke@gmail.com>
2022-05-30 11:51:33 -0700

    Diagnostic logging

--- test/rtos/esp-idf/scheduler/main/scheduler_tasknotify.cpp
+++ test/rtos/esp-idf/scheduler/main/scheduler_tasknotify.cpp
@@ -9,13 +9,18 @@ using namespace estd::literals;
 #if SCHEDULER_APPROACH == SCHEDULER_APPROACH_TASKNOTIFY
 void scheduler_daemon_task(void*)
 {
+    const char* TAG = "scheduler_daemon_task";
+
 //    typedef FunctorTraits::time_point time_point;
+
+    ESP_LOGI(TAG, "start");
 
     for(;;)
     {
         auto duration = scheduler.top_time() - freertos_clock::now();
         ulTaskNotifyTake(pdFALSE, duration.count());
 
+        ESP_LOGI(TAG, "wake");
         scheduler.process();
     }
 }
