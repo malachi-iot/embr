71d8055d37790520d2b589b929aa170491309c8f
Malachi Burke <malachi.burke@gmail.com>
2022-05-30 16:39:46 -0700

    Coming along, but rapid_f reschedule doesn't light up

--- test/rtos/esp-idf/scheduler/main/scheduler.cpp
+++ test/rtos/esp-idf/scheduler/main/scheduler.cpp
@@ -24,13 +24,35 @@ void scheduler_init()
 
     typedef FunctorTraits::time_point time_point;
 
-    auto f = FunctorTraits::make_function([](time_point* wake, time_point current)
+    static int rapid_counter = 0;
+
+    auto rapid_f = FunctorTraits::make_function([](time_point* wake, time_point current)
     {
+        ESP_LOGI(TAG, "rapid_f: %d", rapid_counter);
+
+        if(--rapid_counter > 0)
+        {
+            ESP_LOGD(TAG, "rapid_f: schedule wakeup");
+            *wake += 250ms;
+        }
+    });
+
+    auto f = FunctorTraits::make_function([&](time_point* wake, time_point current)
+    {
+        // TODO: Add modulo operator to chrono proper
+
+        if(counter % 5 == 0)
+        {
+            ESP_LOGI(TAG, "Got here");
+            rapid_counter = 5;
+            scheduler.schedule(*wake + 500ms, rapid_f);
+        }
+
         *wake += 3000ms;
         ESP_LOGI(TAG, "scheduled: counter=%d", counter);
     });
 
-    scheduler.schedule(freertos_clock::now() + 10s, f);
+    scheduler.schedule(freertos_clock::now() + 5s, f);
         
     for(;;)
     {
