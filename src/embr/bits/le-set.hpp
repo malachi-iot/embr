e3cac450987f817f96ea94284c09c68a995dd0dd
Malachi Burke <malachi.burke@gmail.com>
2022-07-26 09:08:16 -0700

    Prepping full feature "high definition" LE setter

--- src/embr/bits/le-set.hpp
+++ src/embr/bits/le-set.hpp
@@ -24,6 +24,66 @@ struct setter<bitpos, length, little_endian, lsb_to_msb, lsb_to_msb,
            !is_subbyte(bitpos, length)> > :
     setter_tag
 {
+    constexpr static int adjuster()
+    {
+        return 0;
+    }
+
+    constexpr static int adjuster(descriptor d)
+    {
+        return 0;
+    }
+
+    template <typename TForwardIt, typename TInt>
+    inline static void set_assist(unsigned& i, TForwardIt& raw, TInt& v)
+    {
+        typedef typename estd::iterator_traits<TForwardIt>::value_type byte_type;
+        constexpr size_t byte_width = byte_size();
+
+        for(; i > byte_width; i -= byte_width)
+        {
+            *raw++ = (byte_type) v;
+            v >>= byte_width;
+        }
+    }
+
+    // Copy/paste & adapted from internal::setter, UNTESTED
+    template <typename TForwardIt, typename TInt>
+    static inline void set(descriptor d, TForwardIt raw, TInt v)
+    {
+        constexpr size_t byte_width = byte_size();
+        const unsigned width = 
+            internal::width_deducer_lsb_to_msb(d);
+
+        typedef typename estd::iterator_traits<TForwardIt>::value_type byte_type;
+        unsigned outside_material = width - d.length;
+        unsigned outside_right_material = outside_material - d.bitpos;
+        unsigned inside_right_material = byte_width - outside_right_material;
+
+        byte_type
+            // mask for first 'raw', for LE that is loosely LSB
+            left_mask = (1 << d.bitpos) - 1,
+            // mask for last 'raw', for LE that is loosely MSB
+            right_mask = (1 << inside_right_material) - 1;
+
+        // first, smallest part of value is written, shifted over by bit position
+        *raw &= left_mask;
+        *raw |= (byte_type) (v << d.bitpos);
+
+        unsigned inside_left_material = byte_width - d.bitpos;
+
+        v >>= inside_left_material;
+
+        // i = remaining bits to store, now that LSB on left side is written
+        // since we check for sub-byte d.length at the top, there is no danger of
+        // rollunder
+        unsigned i = d.length - inside_left_material;
+
+        set_assist(i, ++raw, v);
+
+        *raw &= ~right_mask;
+        *raw |= v;
+    }
 };
 
 /// multi-byte byte boundary version UNTESTED
