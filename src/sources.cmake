28e58c9ec35d1f7ca98dad0ca03e516a26fb8dd8
Malachi Burke <malachi.burke@gmail.com>
2022-09-02 10:48:26 -0700

    Removing embr-lib and estd_lib references

---
+++ src/sources.cmake
@@ -0,0 +1,24 @@
+set(SOURCE_FILES
+    embr/dataport.h
+    embr/dataport.hpp
+    embr/datapump.h
+    embr/datapump.hpp
+    embr/events.h
+
+    embr/exp/netbuf-alloc.h
+
+    embr/netbuf.h
+    embr/netbuf-static.h
+    embr/netbuf-dynamic.h
+    embr/netbuf-reader.h
+    embr/netbuf-writer.h
+
+    embr/observer.h
+
+    embr/scheduler.h
+
+    embr/streambuf.h
+    embr/transport-descriptor.h
+        embr/exp/datapump-v2.h embr/exp/pbuf.h embr/exp/retry-v2.h embr/exp/datapump-core-v2.h embr/exp/dataport-v2.h
+    )
+
