6cf49e99be4e518949fcda1f79b29ec96737b7e9
Malachi Burke <malachi.burke@gmail.com>
2022-09-03 12:52:36 -0700

    Prepping v3 class BE getter

--- src/embr/bits/be-get.hpp
+++ src/embr/bits/be-get.hpp
@@ -63,6 +63,25 @@ struct getter<bitpos, length, big_endian, lsb_to_msb, lsb_to_msb,
 
         v = internal::get_be_lsb_to_msb<TInt>(width, d, raw);
     }
+
+    template <typename TForwardIt, typename TInt>
+    static inline void get_unready(descriptor d, TForwardIt raw, TInt& v)
+    {
+        typedef typename estd::iterator_traits<TForwardIt>::value_type byte_type;
+        constexpr size_t byte_width = sizeof(byte_type) * byte_size();
+
+        v = *raw;
+
+        v >>= d.bitpos;
+
+        // FIX: Probably this math is wrong
+        int i = d.length - d.bitpos;
+
+        get_assist(i, raw, v);
+
+        // FIX: Need to handle remainder byte.  Remember this code generally
+        // assumes multibyte operation
+    }
 };
 
 }
