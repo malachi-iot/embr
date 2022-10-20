0a7c36ca6607a70a9a0113ea047f1a178bac119c
Malachi Burke <malachi.burke@gmail.com>
2022-10-19 23:15:33 -0700

    Brute forcing things into working - and they do work!

--- test/rtos/esp-idf/button/main/gpio.cpp
+++ test/rtos/esp-idf/button/main/gpio.cpp
@@ -5,12 +5,14 @@
 void init_gpio()
 {
     gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_INPUT;
     //disable pull-down mode
-    io_conf.pull_down_en = (gpio_pulldown_t)0;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    //io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
     //disable pull-up mode
-    io_conf.pull_up_en = (gpio_pullup_t)0;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+
     //io_conf.pull_up_en = (pull_mode == GPIO_PULLUP_ONLY || pull_mode == GPIO_PULLUP_PULLDOWN);
     //io_conf.pull_down_en = (pull_mode == GPIO_PULLDOWN_ONLY || pull_mode == GPIO_PULLUP_PULLDOWN);;
     io_conf.pin_bit_mask = PIN_BIT(CONFIG_BUTTON_PIN);
--- test/rtos/esp-idf/button/main/main.cpp
+++ test/rtos/esp-idf/button/main/main.cpp
@@ -2,6 +2,7 @@
 
 #include "esp_log.h"
 #include "esp_system.h"
+#include "esp_task_wdt.h"
 #include "esp_wifi.h"
 
 #include <driver/gpio.h>
@@ -34,18 +35,34 @@ extern "C" void app_main()
 
     Debouncer d;
     bool old_level = false;
+    auto last_now = estd::chrono::freertos_clock::now();
     int counter = 0;
 
+    esp_task_wdt_add(nullptr);
+
+    // Careful of this situation:
+    // https://esp32.com/viewtopic.php?f=2&t=809&start=10
+    // Basically, yield won't yield to IDLE task because we are higher priority and
+    // sleep_for under 10ms works out to be a yield.  In either case, IDLE task can't
+    // feed watchdog and we get a situation
+    // DEBT: To get around this, I've disabled watchdog in IDLE tasks.  That is not great.
+    // What would be better to is to demote this app_main task priority to below IDLE task priority
+    // so that yield() lets IDLE run
     for(;;)
     {
-        const auto duration = 10ms;
-        estd::this_thread::sleep_for(duration);
+        const auto duration = 1ms;
+        // NOTE: Sleep doesn't last NEARLY 1ms!
+        // DEBT: May want to put a workaround into sleep_for for FreeRTOS?  Get into why delay
+        // is so innacurate
+        //estd::this_thread::sleep_for(duration);
+        estd::this_thread::yield();
+
         bool level = gpio_get_level(pin);
         bool state_changed = d.time_passed(duration, level);
 
         if(state_changed)
         {
-            ESP_LOGI(TAG, "state changed: ");
+            ESP_LOGI(TAG, "state changed: %d", d.state() == Debouncer::On);
         }
 
         if(old_level != level)
@@ -55,10 +72,15 @@ extern "C" void app_main()
             old_level = level;
         }
 
-        if((++counter % 500) == 0)
+        auto now = estd::chrono::freertos_clock::now();
+
+        if(now - last_now > 1s)
         {
-            ESP_LOGD(TAG, "counter: %d", counter);
+            ESP_LOGD(TAG, "counter: %d", ++counter);
+            last_now = now;
         }
+
+        esp_task_wdt_reset();
     }
 }
 
