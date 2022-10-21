01efa971cb3d11c75353c539d1456c9d3619326b
Malachi Burke <malachi.burke@gmail.com>
2022-10-21 09:02:55 -0700

    Prep to use scheduler in timer0 handler

--- src/embr/platform/esp-idf/debounce.cpp
+++ src/embr/platform/esp-idf/debounce.cpp
@@ -9,10 +9,12 @@
 #include "timer.h"
 #include "freertos/timer.h"
 
-#include "esp_log.h"
+#include <esp_log.h>
 
 #include <driver/gpio.h>
 #include <driver/timer.h>
+
+#include "../../scheduler.h"
 
 namespace embr { inline namespace esp_idf {
 
@@ -39,6 +41,31 @@ embr::esp_idf::Timer timer(timer_group, timer_idx);
 embr::freertos::timer<> held_timer("held", 3s, false, nullptr, held_callback);
 
 bool low_means_pressed = true;
+
+struct ThresholdImpl
+{
+    typedef estd::chrono::duration<uint32_t, estd::milli> time_point;
+
+    struct value_type
+    {
+        embr::detail::Debouncer* debouncer;
+        time_point wakeup;
+    };
+
+    static inline const time_point& get_time_point(const value_type& v)
+    {
+        return v.wakeup;
+    }
+
+    static bool process(value_type& v, time_point now)
+    {
+        return false;
+    }
+
+    typedef estd::monostate mutex;
+};
+
+static embr::internal::layer1::Scheduler<5, ThresholdImpl> scheduler;
 
 inline void Debouncer::gpio_isr()
 {
