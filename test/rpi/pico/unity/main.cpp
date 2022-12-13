ea9e23d98448a3fc9b5d4ccb77c53b9aeb625cfa
Malachi Burke <malachi.burke@gmail.com>
2022-12-12 16:33:08 -0800

    Init and background threadsafe modes don't seem to help

--- test/rpi/pico/unity/main.cpp
+++ test/rpi/pico/unity/main.cpp
@@ -1,16 +1,32 @@
+#include <estd/ostream.h>
+
+#include <pico/cyw43_arch.h>
 #include <pico/stdio_usb.h>
 
 #include <unity.h>
 
 #include <unit-test.h>
 
-void setUp() {}
+static estd::pico_ostream clog(stdio_usb);
+
+void setUp()
+{
+    //cyw43_arch_enable_sta_mode();
+}
 
 void tearDown() {}
 
 int main()
 {
     stdio_init_all();
+
+    clog << "starting..." << estd::endl;
+
+    if (cyw43_arch_init())
+    {
+        clog << "setUp: failed to initialise" << estd::endl;
+        return -1;
+    }
 
     UNITY_BEGIN();
     test_bits();
