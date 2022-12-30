7fdc6230c0299e3594e5f0fd22da4fd6ea3c84c3
Malachi Burke <malachi.burke@gmail.com>
2022-12-30 12:17:24 -0800

    Compile-time segregation of SDK v5 code

--- test/rtos/esp-idf/components/testable/test/timer.cpp
+++ test/rtos/esp-idf/components/testable/test/timer.cpp
@@ -1,7 +1,6 @@
 #include "unity.h"
 
 #include <embr/platform/esp-idf/timer.h>
-#include <embr/platform/esp-idf/gptimer.h>
 
 using namespace embr::esp_idf;
 
@@ -10,7 +9,11 @@ TEST_CASE("general purpose timer v4", "[gptimer v4]")
     v4::Timer timer(TIMER_GROUP_0, TIMER_0);
 }
 
+#if ESTD_IDF_VER > ESTD_IDF_VER_5_0_0
+#include <embr/platform/esp-idf/gptimer.h>
+
 TEST_CASE("general purpose timer v5", "[gptimer v5]")
 {
     v5::Timer timer;
 }
+#endif
