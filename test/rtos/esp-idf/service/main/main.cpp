337904b7863b204971e3d65a4f86f4488511c2f4
Malachi Burke <malachi.burke@gmail.com>
2023-01-24 20:18:16 -0800

    Boilerplate

---
+++ test/rtos/esp-idf/service/main/main.cpp
@@ -0,0 +1,27 @@
+#include <esp-helper.h>
+
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_task_wdt.h"
+#include "esp_wifi.h"
+
+#include <driver/gpio.h>
+
+#include <estd/chrono.h>
+#include <estd/optional.h>
+#include <estd/thread.h>
+
+
+extern "C" void app_main()
+{
+    const char* TAG = "app_main";
+
+#if CONFIG_WIFI_ENABLED
+#ifdef FEATURE_IDF_DEFAULT_EVENT_LOOP
+    wifi_init_sta();
+#else
+    wifi_init_sta(event_handler);
+#endif
+#endif
+}
+
