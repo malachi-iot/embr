9a52cdbe65fbf32b2ab5119a7ddee2a4f28ced62
Malachi Burke <malachi.burke@gmail.com>
2023-01-25 21:29:30 -0800

    Adding in auto-generating version

---
+++ src/embr/version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <estd/port/arch/version.h>
+
+#define EMBR_VER_MAJOR     0
+#define EMBR_VER_MINOR     2
+#define EMBR_VER_PATCH     0
+
+#define EMBR_VERSION  ESTD_BUILD_SEMVER(0, 2, 0)
+#define EMBR_VERSION_STR "0.2.0"
