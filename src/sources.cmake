55a7985d516f08f15ec7c52555e79de825c7599d
Malachi Burke <malachi.burke@gmail.com>
2023-01-31 10:22:44 -0800

    Guarding against pre c++11

--- src/sources.cmake
+++ src/sources.cmake
@@ -13,9 +13,13 @@ set(INCLUDE_FILES
     embr/exp/pbuf.h embr/exp/retry-v2.h embr/exp/dataport-v2.h
     )
 
-set(SOURCE_FILES
-    embr/service/service_v1.cpp
-    )
+# NOTE: Really, most of embr requires c++11 onward
+
+if(NOT CMAKE_CXX_STANDARD EQUAL 98)
+    set(SOURCE_FILES
+        embr/service/service_v1.cpp
+        )
+endif()
 
 set(ESP_IDF_SOURCE_FILES ${SOURCE_FILES}
     embr/platform/esp-idf/debounce.cpp
