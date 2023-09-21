0997c3c5a1a5957667ed6046d269661f99d97e52
Malachi Burke <malachi.burke@gmail.com>
2023-09-21 09:04:06 -0700

    Switching Catch2 usage to FetchContent rather than submodule

--- tools/cmake/setvars.cmake
+++ tools/cmake/setvars.cmake
@@ -1,3 +1,7 @@
 get_filename_component(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 set(EXT_DIR ${ROOT_DIR}/ext)
 message(DEBUG "embr: EXT_DIR=${EXT_DIR}")
+
+set(ESTD_DIR ${EXT_DIR}/estdlib/src)
+
+include(${EXT_DIR}/estdlib/tools/cmake/fetchcontent.cmake)
