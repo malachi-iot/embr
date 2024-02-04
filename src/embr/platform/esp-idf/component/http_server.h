2facd0102e5373a8c3f9c6dd1421a0a238330b52
Malachi Burke <malachi.burke@gmail.com>
2024-02-04 11:44:50 -0800

    Whipped up minimal buffered streambuf
    Not well tested

--- src/embr/platform/esp-idf/component/http_server.h
+++ src/embr/platform/esp-idf/component/http_server.h
@@ -52,13 +52,87 @@ template <class Streambuf>
 class simple_buffer_ostreambuf :
     public estd::internal::impl::streambuf_base<typename Streambuf::traits_type>
 {
-    Streambuf streambuf_;
+    using base_type = estd::internal::impl::streambuf_base<typename Streambuf::traits_type>;
 
     // TODO: Make this an is-a not a has-a so we can do both istreambuf and ostreambuf
-    estd::layer1::string<64, false> buf_;
+    Streambuf streambuf_;
+
+    static constexpr int len_ = 64;
+
+    // TODO: Make this a string type we pass in somehow
+    estd::layer1::string<len_, false> buf_;
+
+    constexpr unsigned remaining() const
+    {
+        return buf_.max_size() - buf_.size();
+    }
+
+    // DEBT: Finish up string/allocated_array underlying full implementation
+    constexpr bool full() const
+    {
+        return remaining() == 0;
+    }
+
+    void ll_sync()
+    {
+        streambuf_.xsputn(buf_.data(), buf_.size());
+        buf_.clear();
+    }
+
+public:
+    using typename base_type::int_type;
+    using typename base_type::char_type;
+    using typename base_type::traits_type;
+
+protected:
+    int sync()
+    {
+        if(buf_.empty()) return 0;
+
+        ll_sync();
+
+        return 0;
+    }
+
+    int_type overflow(int_type ch)
+    {
+        if(full())
+            ll_sync();
+
+        if(traits_type::not_eof(ch))
+        {
+            buf_ += traits_type::to_char_type(ch);
+        }
+
+        return ch;
+    }
+
+    estd::streamsize xsputn(const char_type* s, estd::streamsize count)
+    {
+        const estd::streamsize maximum = remaining();
+
+        if(count > maximum) count = maximum;
+        
+        buf_.append(s, count);
+
+        if(full())
+            ll_sync();
+
+        return count;
+    }
 
 public:
     ESTD_CPP_FORWARDING_CTOR_MEMBER(simple_buffer_ostreambuf, streambuf_)
+
+    char_type* pbase() const { return buf_.data(); }
+    char_type* pptr() const { return pbase() + buf_.size(); }
+    char_type* epptr() const { return pbase() + buf_.max_size(); }
+
+    int_type sputc(char_type ch)
+    {
+        buf_ += ch;
+        return ch;
+    }
 };
 
 }
@@ -72,5 +146,13 @@ template <class CharT, class CharTraits = estd::char_traits<CharT> >
 using basic_httpd_ostream = estd::detail::basic_ostream<basic_httpd_ostreambuf<CharT, CharTraits> >;
 
 using httpd_ostream = basic_httpd_ostream<char>;
+
+template <class Streambuf>
+using basic_simple_buffer_ostreambuf = estd::internal::streambuf<
+    impl::simple_buffer_ostreambuf<Streambuf> >;
+
+template <class Streambuf>
+using buffered_ostream = estd::detail::basic_ostream<
+    basic_simple_buffer_ostreambuf<Streambuf> >;
 
 }}
