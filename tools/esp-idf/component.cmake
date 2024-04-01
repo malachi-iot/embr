d60d3fd07205d57840ce5dec03d6d85e5f3363bf
Malachi Burke <malachi.burke@gmail.com>
2024-04-01 09:19:55 -0700

    Adding name override as we transition from estdlib -> estd name

--- tools/esp-idf/component.cmake
+++ tools/esp-idf/component.cmake
@@ -1,7 +1,12 @@
 set(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../..)
 set(SRC_DIR ${ROOT_DIR}/src)
 
-set(COMPONENT_REQUIRES estdlib driver esp_event esp_timer)
+# As we transition from 'estdlib' to 'estd' naming
+if(NOT DEFINED ESTD_COMPONENT_NAME)
+	set(ESTD_COMPONENT_NAME estdlib)
+endif()
+
+set(COMPONENT_REQUIRES ${ESTD_COMPONENT_NAME} driver esp_event esp_timer)
 
 # Get ESP_IDF_SOURCE_FILES from here
 include(${SRC_DIR}/sources.cmake)
