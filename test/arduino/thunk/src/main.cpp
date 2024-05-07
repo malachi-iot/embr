9eca4dd484888fb2e8bcff2d232af612d4d87118
Malachi Burke <malachi.burke@gmail.com>
2024-05-07 09:47:40 -0700

    Prepping ISR thunk test
    pio picks up two bipbuffer.c's it seems

--- test/arduino/thunk/src/main.cpp
+++ test/arduino/thunk/src/main.cpp
@@ -2,11 +2,22 @@
 
 #include <embr/exp/thunk.h>
 
+struct Mutex
+{
+    static inline void lock_push() { cli(); }
+    static inline void unlock_push() { sei(); }
+    static inline void lock_pop() { cli(); }
+    static inline void unlock_pop() { sei(); }
+};
+
+embr::experimental::layer1::Thunk<128, Mutex> thunk;
+
 void setup()
 {
 }
 
 void loop()
 {
+    thunk.invoke();
 }
 
