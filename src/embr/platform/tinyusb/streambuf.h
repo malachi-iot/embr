0d95b3547a790b261d0350d85ac06c8232a4852e
Malachi Burke <malachi.burke@gmail.com>
2024-06-18 08:01:17 -0700

    Boilerplate

--- src/embr/platform/tinyusb/streambuf.h
+++ src/embr/platform/tinyusb/streambuf.h
@@ -1,3 +1,10 @@
 #pragma once
 
 #include "streambuf/out.h"
+
+namespace embr { namespace tinyusb { inline namespace v1 {
+
+//template <class Char, class CharTraits = estd::char_traits<Char> >
+//using basic_ocdc_streambuf = estd::internal::streambuf<impl::opbuf_streambuf<CharTraits> >;
+
+}}}
--- src/embr/platform/tinyusb/streambuf/out.h
+++ src/embr/platform/tinyusb/streambuf/out.h
@@ -4,6 +4,39 @@
 
 namespace embr { namespace tinyusb { inline namespace v1 {
 
+namespace impl {
 
+template <class CharTraits>
+struct ocdc_streambuf
+{
+    using traits_type = CharTraits;
+
+    typedef typename traits_type::char_type char_type;
+    typedef typename traits_type::int_type int_type;
+
+protected:
+    int_type overflow(int_type ch = traits_type::eof())
+    {
+        return {};
+    }
+
+public:
+    // DEBT: Feels like a boilerplate that owning streambuf can
+    // implement
+    int_type sputc(char_type ch)
+    {
+        const int_type _ch = traits_type::to_int_type(ch);
+        int_type result = overflow(_ch);
+        if(result == traits_type::eof()) return traits_type::eof();
+        return _ch;
+    }
+
+    estd::streamsize xsputn(const char_type* s, estd::streamsize count)
+    {
+        return {};
+    }
+};
+
+}
 
 }}}
