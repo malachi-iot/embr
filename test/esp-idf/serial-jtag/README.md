a3cb5fb5b909d2ff32bec24cedffce734f539d05
Malachi Burke <malachi.burke@gmail.com>
2024-06-20 06:55:36 -0700

    Works, with caveats

--- test/esp-idf/serial-jtag/README.md
+++ test/esp-idf/serial-jtag/README.md
@@ -1,3 +1,10 @@
+
+# 1. Design Goals
+
+# 2. Infrastructure
+
+# 3. Opinions & Observations
+
 See
 https://www.esp32.com/viewtopic.php?t=38971
 
@@ -5,6 +12,11 @@ For (more undocumented, similar to TinyUSB, thanks) ways to use the ESP32C6 (and
 
 Section 31 of datasheet [1] goes into detail on the limited USB peripheral
 
+Seems that one can't inhibit log output on one console without inhibiting it on the other [2]
+
+# 4. Conclusions
+
 # References
 
 1. https://www.espressif.com/sites/default/files/documentation/esp32-c6_technical_reference_manual_en.pdf
+2. https://esp32.com/viewtopic.php?t=29237
--- test/esp-idf/serial-jtag/main/main.cpp
+++ test/esp-idf/serial-jtag/main/main.cpp
@@ -1,16 +1,33 @@
+#include <esp_log.h>
+
 #include <estd/ostream.h>
+#include <estd/thread.h>
 
 #include "out.h"
 
+// DEBT: Make std::chrono work with estd::this_thread
+using namespace estd::chrono_literals;
 using ostream = estd::detail::basic_ostream<ocdc_streambuf>;
 
 extern "C" void app_main(void)
 {
+    static const char* TAG = "app_main";
+
     static auto config = USB_SERIAL_JTAG_DRIVER_CONFIG_DEFAULT();
 
     ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&config));
 
+    unsigned counter = 0;
     ostream out;
 
-    out << "Hello" << estd::endl;
+    for(;;)
+    {
+        ESP_LOGI(TAG, "counter: %u", counter);
+
+        out << "Hello: " << counter << estd::endl;
+
+        ++counter;
+
+        estd::this_thread::sleep_for(1000ms);
+    }
 }
