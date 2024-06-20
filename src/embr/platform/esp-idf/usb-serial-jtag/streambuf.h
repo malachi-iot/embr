be8bba61de615dcf2ad83bd6489d5425a52fecaa
Malachi Burke <malachi.burke@gmail.com>
2024-06-20 07:14:04 -0700

    Importing usb-serial-jtag streambuf into API

--- src/embr/platform/esp-idf/usb-serial-jtag/streambuf.h
+++ src/embr/platform/esp-idf/usb-serial-jtag/streambuf.h
@@ -3,3 +3,65 @@
 #include <estd/streambuf.h>
 
 #include <driver/usb_serial_jtag.h>
+
+namespace embr::esp_idf {
+
+// See https://github.com/espressif/esp-idf/blob/v5.2.2/components/driver/usb_serial_jtag/include/driver/usb_serial_jtag.h
+
+namespace impl {
+
+/*
+ * TODO: Ultimately this particular streambuf will be combined I/O variety
+template <class CharTraits>
+struct iusj_streambuf : estd::internal::impl::streambuf_base<CharTraits>
+{
+    streamsize xsgetn(char_type* s, streamsize count)
+    {
+
+    }
+};  */
+
+template <class CharTraits>
+struct usj_streambuf : estd::internal::impl::streambuf_base<CharTraits>
+{
+    using base_type = estd::internal::impl::streambuf_base<CharTraits>;
+
+    using typename base_type::traits_type;
+    using typename base_type::char_type;
+    using typename base_type::int_type;
+
+    // DEBT: Revisit how streambufs are supposed to do blocking/nonblocking
+
+protected:
+/*
+    int_type overflow(int_type ch = traits_type::eof())
+    {
+        return {};
+    }   */
+
+public:
+    int_type sputc(char_type ch)
+    {
+        //const int_type _ch = traits_type::to_int_type(ch);
+        //int_type result = overflow(_ch);
+        int result = usb_serial_jtag_write_bytes(&ch, 1, 0);
+        //if(result == traits_type::eof()) return traits_type::eof();
+        //return _ch;
+        return result == 1 ?
+            traits_type::to_int_type(ch) :
+            traits_type::eof();
+    }
+
+    estd::streamsize xsputn(const char_type* s, estd::streamsize count)
+    {
+        return usb_serial_jtag_write_bytes(s, count, 0);
+    }
+};
+
+}
+
+template <class Char, class CharTraits = estd::char_traits<char> >
+using ousj_streambuf = estd::detail::streambuf<
+    impl::usj_streambuf<CharTraits> >;
+
+}
--- test/esp-idf/serial-jtag/main/main.cpp
+++ test/esp-idf/serial-jtag/main/main.cpp
@@ -3,11 +3,20 @@
 #include <estd/ostream.h>
 #include <estd/thread.h>
 
-#include "out.h"
+#include <embr/platform/esp-idf/usb-serial-jtag/streambuf.h>
 
 // DEBT: Make std::chrono work with estd::this_thread
 using namespace estd::chrono_literals;
+
+//template <class Streambuf, unsigned N = 64>
+//using out_bipbuf_streambuf = detail::streambuf<impl::out_buffered_bipbuf<Streambuf, N> >;
+
+using ocdc_streambuf = embr::esp_idf::ousj_streambuf<char>;
+
+using sb2 = estd::internal::out_bipbuf_streambuf<ocdc_streambuf>;
+
 using ostream = estd::detail::basic_ostream<ocdc_streambuf>;
+using ostream2 = estd::detail::basic_ostream<sb2>;
 
 extern "C" void app_main(void)
 {
@@ -19,12 +28,14 @@ extern "C" void app_main(void)
 
     unsigned counter = 0;
     ostream out;
+    ostream2 out2;
 
     for(;;)
     {
         ESP_LOGI(TAG, "counter: %u", counter);
 
         out << "Hello: " << counter << estd::endl;
+        out << "Hello2: " << counter << estd::endl;
 
         ++counter;
 
