c2a736e231e4689881179bf946c86e359b0a4dc8
Malachi Burke <malachi.burke@gmail.com>
2024-07-25 08:08:04 -0700

    Fleshing out automated project metadata gen

--- tools/cmake/version.cmake
+++ tools/cmake/version.cmake
@@ -1,31 +1,31 @@
-# Depends on setvars
-
-# FIX: Copy/pasted in from estdlib - make this into a function that
+# DEBT: Copy/pasted in from estdlib - make this into a function that
 # estdlib eventually defines so that we can share code
 
-string(TOUPPER ${PROJECT_NAME} PROJECT_NAME_UPPER)
+function(configure_version ROOT_DIR VERSION_DIR)
+    set(WORKING_DIR ${ROOT_DIR}/tools/cmake/in)
 
-set(WORKING_DIR ${CMAKE_CURRENT_LIST_DIR}/in)
+    message(STATUS "version.cmake: ${CMAKE_CURRENT_SOURCE_DIR} / ${WORKING_DIR}")
+    
+    # string(TOUPPER ${PROJECT_NAME} PROJECT_NAME_UPPER)
 
-message(DEBUG "version.cmake: ${CMAKE_CURRENT_SOURCE_DIR} / ${WORKING_DIR}")
+    configure_file(
+        ${WORKING_DIR}/version.in.h
+        ${VERSION_DIR}/version.h)
 
-# DEBT: 'port' is not a great destination
-configure_file(
-    ${WORKING_DIR}/version.in.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/estd/port/version.h)
+    # esp-idf: For internal testing
+    configure_file(
+        ${WORKING_DIR}/idf_component.in.yml
+        ${ROOT_DIR}/tools/esp-idf/components/estdlib/idf_component.yml)
 
-# esp-idf: For internal testing
-configure_file(
-    ${WORKING_DIR}/idf_component.in.yml
-    ${ROOT_DIR}/tools/esp-idf/components/estdlib/idf_component.yml)
+    # esp-idf: For general use
+    configure_file(
+        ${WORKING_DIR}/idf_component.in.yml
+        ${ROOT_DIR}/idf_component.yml)
 
-# esp-idf: For general use
-configure_file(
-    ${WORKING_DIR}/idf_component.in.yml
-    ${ROOT_DIR}/idf_component.yml)
+    # For platformio
+    configure_file(
+        ${WORKING_DIR}/library.in.json
+        ${ROOT_DIR}/library.json)
+        
+endfunction()
 
-# For platformio
-configure_file(
-    ${WORKING_DIR}/library.in.json
-    ${ROOT_DIR}/library.json)
-	
