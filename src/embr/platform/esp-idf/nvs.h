b98c6b2d257c7454d9f4c68f8cfb324d2ec1be71
Malachi Burke <malachi.burke@gmail.com>
2024-09-25 09:14:55 -0700

    Adding commit, erase calls and untested scoped_guard

--- src/embr/platform/esp-idf/nvs.h
+++ src/embr/platform/esp-idf/nvs.h
@@ -7,7 +7,7 @@
 
 // NOTE: esp-idf has its own C++ thing going on with full unique_ptr and virtual
 // methods.  We're going a different toute
-namespace embr { namespace esp_idf { namespace nvs {
+namespace embr { namespace esp_idf { namespace nvs { inline namespace v1 {
 
 // NOTE: Since they have varieties of handles in their c++ implementation, 
 // I expect we'll need some template tricks or namespace segregation.  Not doing that now though.
@@ -19,6 +19,21 @@ public:
     esp_err_t open(const char* namespace_name, nvs_open_mode_t open_mode)
     {
         return nvs_open(namespace_name, open_mode, &h);
+    }
+
+    esp_err_t commit() const
+    {
+        return nvs_commit(h);
+    }
+
+    esp_err_t erase() const
+    {
+        return nvs_erase_all(h);   
+    }
+
+    esp_err_t erase(const char* key) const
+    {
+        return nvs_erase_key(h, key);
     }
 
     esp_err_t get_blob(const char* key, void* out_value, std::size_t* sz)
@@ -79,11 +94,18 @@ public:
     void close()
     {
         nvs_close(h);
+
+        // DEBT: Do a strict-ish mode reassignment of 'h'
     }
 
     operator nvs_handle_t() const { return h; }
 };
 
+// Almost same as regular handle, mainly indicates to compiler
+// that write mode operations are happening for this guy
+struct WriteHandle : Handle
+{
+};
 
 template <class T>
 esp_err_t get(Handle h, const char* key, T* blob)
@@ -116,7 +138,7 @@ esp_err_t set(Handle h, const char* key, T* blob)
 
 
 
-}}}
+}}}}
 
 namespace embr { namespace internal {
 
@@ -124,6 +146,36 @@ template <>
 struct scoped_guard_traits<esp_idf::nvs::Handle>
 {
     using status_type = esp_err_t;
+};
+
+template <>
+struct scoped_guard_traits<esp_idf::nvs::v1::WriteHandle>
+{
+    using status_type = esp_err_t;
+};
+
+
+// UNTESTED
+template <scoped_guard_fail_action a>
+class scoped_guard<esp_idf::nvs::v1::WriteHandle, a>
+{
+    using scoped_type = esp_idf::nvs::v1::WriteHandle;
+
+    scoped_type h;
+
+    // DEBT: accomodate 'a' and handle errors accordingly
+
+public:
+    scoped_guard(const char* namespace_name)
+    {
+        h.open(namespace_name, NVS_READWRITE);
+    }
+
+    ~scoped_guard()
+    {
+        h.commit();
+        h.close();
+    }
 };
 
 }}
