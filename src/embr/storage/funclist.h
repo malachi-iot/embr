3aae1ecc2626ac1c0cb2502994a2090ff497ab13
Malachi Burke <malachi.burke@gmail.com>
2024-10-04 14:04:56 -0700

    feat(objlist): splitting out impl
    In anticipation of a layer0/global flavor

--- src/embr/storage/funclist.h
+++ src/embr/storage/funclist.h
@@ -13,37 +13,18 @@ struct funclist
 {
     using value_type = Element;
     using pointer = Element*;
-};
-
-}
-
-template <class F, class Objlist>
-class funclist
-{
-    using objlist_type = Objlist;
-
-    Objlist* list_;
-
-    using fnptr = estd::detail::impl::function_fnptr2<F>;
-    using pointer = typename objlist_type::pointer;
 
     pointer head_ = nullptr;
-
-public:
-    constexpr funclist(objlist_type* objlist) :
-        list_{objlist}
-    {
-
-    }
+    using fnptr = estd::detail::impl::function_fnptr2<F>;
 
     constexpr bool empty() const { return head_ == nullptr; }
 
-    template <class F2>
-    void operator+=(F2&& f)
+    template <class Objlist, class F2>
+    void add(Objlist& list, F2&& f)
     {
         using model = typename fnptr::template model<F2>;
 
-        pointer p = list_->alloc(nullptr, sizeof(model));
+        pointer p = list.alloc(nullptr, sizeof(model));
 
         if(p == nullptr) return;
 
@@ -59,19 +40,51 @@ public:
         }
     }
 
-    template <class ...Args>
-    void fire(Args&&...args)
+    template <class Objlist, class ...Args>
+    void fire(Objlist& list, Args&&...args)
     {
         using model = typename fnptr::model_base;
 
         // DEBT: Combining everything into objlist makes knowing which 'walk' to
         // call a little confusing
-        list_->walk(head_, [&](pointer p, pointer)
+        list.walk(head_, [&](pointer p, pointer)
         {
             auto m = (model*)p->data();
 
             m->operator()(std::forward<Args>(args)...);
         });
+    }
+};
+
+}
+
+template <class F, class Objlist>
+class funclist
+{
+    using objlist_type = Objlist;
+    using value_type = typename objlist_type::value_type;
+    using pointer = typename objlist_type::pointer;
+
+    Objlist* list_;
+    impl::funclist<F, value_type> impl_;
+
+public:
+    constexpr funclist(objlist_type* objlist) :
+        list_{objlist}
+    {
+
+    }
+
+    template <class F2>
+    void operator+=(F2&& f)
+    {
+        impl_.add(*list_, std::forward<F2>(f));
+    }
+
+    template <class ...Args>
+    void fire(Args&&...args)
+    {
+        impl_.fire(*list_, std::forward<Args>(args)...);
     }
 };
 
