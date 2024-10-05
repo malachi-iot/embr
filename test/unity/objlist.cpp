21a44d0536ec337ebb7c3a41166dd3c13e936952
Malachi Burke <malachi.burke@gmail.com>
2024-10-04 18:16:38 -0700

    test: prepping objlist tests

---
+++ test/unity/objlist.cpp
@@ -0,0 +1,12 @@
+#include "unit-test.h"
+
+#include <embr/storage/funclist.h>
+
+#ifdef ESP_IDF_TESTING
+TEST_CASE("objlist", "[objlist]")
+#else
+void test_objlist()
+#endif
+{
+
+}
--- test/unity/sources.cmake
+++ test/unity/sources.cmake
@@ -1,6 +1,7 @@
 set(SOURCES
         bits.cpp
         lwip.cpp
+        objlist.cpp
         observer.cpp
         setup.cpp
         word.cpp
