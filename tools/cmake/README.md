6dfef7b85887ebcedaec368faafe6209d2efe346
Malachi Burke <malachi.burke@gmail.com>
2024-11-05 07:19:15 -0800

    build: using CPM to acquire Catch2

---
+++ tools/cmake/README.md
@@ -0,0 +1,3 @@
+CPM file acquired via
+
+wget -O CPM.cmake https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/get_cpm.cmake
--- tools/cmake/setvars.cmake
+++ tools/cmake/setvars.cmake
@@ -1,7 +1,11 @@
+include_guard(GLOBAL)
+
 get_filename_component(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
+set(EMBR_ROOT_DIR ${ROOT_DIR})
 set(EXT_DIR ${ROOT_DIR}/ext)
 message(DEBUG "embr: EXT_DIR=${EXT_DIR}")
 
 set(ESTD_DIR ${EXT_DIR}/estdlib/src)
 
 include(${EXT_DIR}/estdlib/tools/cmake/fetchcontent.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
