78c8ed3c1fc2d08ff147c84bd6d67cfa5a76dac0
Malachi Burke <malachi.burke@gmail.com>
2025-01-01 11:11:17 -0800

    feat(word): further clumsy == handling

--- src/embr/internal/word/v2/operators.h
+++ src/embr/internal/word/v2/operators.h
@@ -15,6 +15,7 @@ constexpr estd::enable_if_t<!(o & word_options::implicit), bool> operator ==(
     return l.value() == r;
 }
 
+
 template <size_t bits1, size_t bits2, word_options o1, word_options o2>
 constexpr bool operator ==(const word<bits1, o1>& l, const word<bits2, o2>& r)
 {
@@ -22,3 +23,19 @@ constexpr bool operator ==(const word<bits1, o1>& l, const word<bits2, o2>& r)
 }
 
 }}
+
+
+namespace embr {
+
+// DEBT: Clumsy stuff here - hopefully we can refactor this into embr::detail::v2::word
+
+
+template <size_t bits, v2::word_options o>
+// Compiler gets annoyed when implicit is active here since it identifies two viable == paths
+constexpr estd::enable_if_t<!(o & v2::word_options::implicit), bool>
+operator==(const internal::word_v2_base<bits, o>& lhs, const typename internal::word_v2_base<bits, o>::type& rhs)
+{
+    return lhs.equals(rhs);
+}
+
+}
--- src/embr/internal/word/v2/word.h
+++ src/embr/internal/word/v2/word.h
@@ -437,25 +437,3 @@ public:
 #endif
 
 }}
-
-
-namespace embr {
-
-// DEBT: Clumsy stuff here - hopefully we can refactor this into embr::detail::v2::word
-
-template <size_t bits, v2::word_options o>
-// Compiler gets annoyed when implicit is active here since it identifies two viable == paths
-constexpr estd::enable_if_t<!(o & v2::word_options::implicit), bool>
-    operator==(const internal::word_v2_base<bits, o>& lhs, const typename internal::word_v2_base<bits, o>::type& rhs)
-{
-    return lhs.equals(rhs);
-}
-
-template <size_t bits, v2::word_options o, uint32_t padding>
-constexpr bool operator==(const v2::word<bits, o, padding>& lhs, const v2::word<bits, o, padding>& rhs)
-{
-    return lhs.equals(rhs);
-}
-
-
-}
