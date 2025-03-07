da54415334d17ca825ba253ccfc8c1199dbf6d5a
Malachi Burke <malachi.burke@gmail.com>
2025-03-07 09:47:56 -0800

    feat: raw (lower level) profiler test = OK

--- src/embr/platform/esp-idf/esp_timer.h
+++ src/embr/platform/esp-idf/esp_timer.h
@@ -1,6 +1,8 @@
 // https://docs.espressif.com/projects/esp-idf/en/v4.4.2/esp32/api-reference/system/esp_timer.html
 #pragma once
 
+#include <estd/chrono.h>
+
 #include <esp_timer.h>
 
 namespace embr { namespace esp_idf {
@@ -10,14 +12,14 @@ class esp_timer
 
 };
 
-namespace clock {
+namespace chrono {
 
 // Special case of pseudo estd::chrono::clock type, returning raw values instead of standard
 // time_point/duration.  Useful for scenarios where:
 // 1. Extra concerned that optimizer won't remove c++ layers
 // 2. Direct reference to uint64_t us is convenient without .count() call
 // Designed to be used in conjunction with embr::Profiler
-struct esp_timer
+struct timer
 {
     using rep = estd::chrono::internal::micro_rep;
     //typedef micro period;
--- test/unity/misc.cpp
+++ test/unity/misc.cpp
@@ -7,15 +7,32 @@ using namespace embr;
 
 static void test_profiler()
 {
+#if ESP_PLATFORM
     using clock = estd::chrono::esp_clock;
+#else
+    // DEBT: Use high resolution clock here
+    using clock = estd::chrono::system_clock;
+#endif
 
     Profiler<clock> p;
 
     uint64_t elapsed = p.mark().count();
 
     TEST_ASSERT_GREATER_THAN(0, elapsed);
+}
 
-    // TODO: test embr::esp_idf::chrono::esp_timer variety
+
+static void test_profiler_raw()
+{
+#if ESP_PLATFORM
+    using clock = embr::esp_idf::chrono::timer;
+
+    Profiler<clock> p;
+
+    uint64_t elapsed = p.mark();
+
+    TEST_ASSERT_GREATER_THAN(0, elapsed);
+#endif
 }
 
 #ifdef ESP_IDF_TESTING
@@ -25,4 +42,5 @@ void test_misc()
 #endif
 {
     RUN_TEST(test_profiler);
+    RUN_TEST(test_profiler_raw);
 }
