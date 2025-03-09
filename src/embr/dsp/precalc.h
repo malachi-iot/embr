1e21bd898ebab38e52a9042533980af70e8df906
Malachi Burke <malachi.burke@gmail.com>
2025-03-09 00:11:32 -0800

    feat: quarter mode sin precalc shaping up

--- src/embr/dsp/precalc.h
+++ src/embr/dsp/precalc.h
@@ -6,30 +6,97 @@
 
 namespace embr { namespace dsp { inline namespace v1 {
 
-template <typename T, estd::size_t N>
-void init_sin_table(estd::span<T, N> table)
+enum precalc_modes
 {
-    T* data = table.data();
-    for(int i = 0; i < N; i++, data++)
+    PRECALC_FULL,
+    PRECALC_HALF,
+    PRECALC_QUART,
+};
+
+namespace detail {
+
+template <precalc_modes>
+struct precalc;
+
+template <>
+struct precalc<PRECALC_HALF>
+{
+    template <typename T, estd::size_t N>
+    void static init_sin(estd::span<T, N> table)
     {
-        double v = i * M_PI / N;
-        *data = sin(v);
+        T* data = table.data();
+        for(int i = 0; i < N; i++, data++)
+        {
+            const auto v = i * M_PI / N;
+            *data = std::sin(v);
+        }
     }
+
+    template <typename T, estd::size_t N>
+    constexpr static T sin(const estd::span<T, N>& table, T v)
+    {
+        constexpr unsigned mask = N * 2 - 1;
+
+        unsigned i = std::round(v * N / M_PI);
+
+        i &= mask;
+
+        if(i < N)
+            return table[i];
+        else
+            return -table[i - N];
+    }
+};
+
+
+template <>
+struct precalc<PRECALC_QUART>
+{
+    template <typename T, estd::size_t N>
+    void static init_sin(estd::span<T, N> table)
+    {
+        T* data = table.data();
+        for(int i = 0; i < N; i++, data++)
+        {
+            const auto v = i * M_PI / (2.0 * N);
+            *data = std::sin(v);
+        }
+    }
+
+    template <typename T, estd::size_t N>
+    constexpr static T sin(const estd::span<T, N>& table, T v)
+    {
+        constexpr unsigned mask = N * 4 - 1;
+
+        int i = std::round(v * 2 * N / M_PI);
+
+        i &= mask;
+
+        if(i < N)
+            return table[i];
+        else if(i < N * 2)
+            //return table[1 - (i - N) + N];
+            return table[1 - i + N * 2];
+        else if(i < N * 3)
+            return -table[i - N * 2];
+        else
+            //return -table[N - 1 - (i - N * 3)];
+            return -table[1 - i + N * 4];
+    }
+};
+
 }
 
-template <typename T, estd::size_t N>
+template <precalc_modes mode = PRECALC_HALF, typename T, estd::size_t N>
+void init_sin_table(estd::span<T, N> table)
+{
+    detail::precalc<mode>::init_sin(table);
+}
+
+template <precalc_modes mode = PRECALC_HALF, typename T, estd::size_t N>
 constexpr T sin_lookup(const estd::span<T, N>& table, T v)
 {
-    constexpr unsigned mask = N * 2 - 1;
-
-    unsigned i = std::round(v * N / M_PI);
-
-    i &= mask;
-
-    if(i < N)
-        return table[i];
-    else
-        return -table[i - N];
+    return detail::precalc<mode>::sin(table, v);
 }
 
 void init_sin_table();
--- test/catch/dsp-test.cpp
+++ test/catch/dsp-test.cpp
@@ -4,10 +4,10 @@
 
 using namespace embr;
 
-template <class T, std::size_t N>
+template <dsp::v1::precalc_modes mode = dsp::v1::PRECALC_HALF, class T, std::size_t N>
 void compare(const estd::span<T, N>& t, T v)
 {
-    T v1 = dsp::v1::sin_lookup(t, v);
+    T v1 = dsp::v1::sin_lookup<mode>(t, v);
     T v1_ = sin(v);
 
     CAPTURE(v1, v1_, v);
@@ -31,4 +31,12 @@ TEST_CASE("dsp")
     compare(t, 32.0);
 
     compare(t2, 0.2f);
+
+    dsp::v1::init_sin_table<dsp::PRECALC_QUART>(t2);
+
+    compare<dsp::PRECALC_QUART>(t2, 0.2f);
+    compare<dsp::PRECALC_QUART>(t2, 3.0f);
+    compare<dsp::PRECALC_QUART>(t2, 6.0f);
+    compare<dsp::PRECALC_QUART>(t2, 9.0f);
+    compare<dsp::PRECALC_QUART>(t2, 32.0f);
 }
