08e8361b5c58f4fb8ad7eefbe6e44623411f82db
Malachi Burke <malachi.burke@gmail.com>
2025-03-09 13:19:52 -0700

    feat(dsp): adding global static vs global pointer config
    Useful if you want to experiment with SPIRAM etc
    Not well tested

--- src/dsp.cpp
+++ src/dsp.cpp
@@ -4,14 +4,25 @@ namespace embr { namespace dsp { inline namespace v1 {
 
 #if FEATURE_EMBR_DSP_PRECALC_TABLE
 namespace detail {
+#if FEATURE_EMBR_DSP_PRECALC_TABLE_STATIC
 float sin_table[EMBR_DSP_PRECALC_TABLE_SZ];
+#else
+float* sin_table;
+#endif
 //float cos_table[EMBR_DSP_PRECALC_TABLE_SZ];
 }
 
-void init_sin_table()
+static void init_sin_table_(float* table)
 {
-    detail::precalc<PRECALC_DEFAULT>::init_sin(estd::span<float, EMBR_DSP_PRECALC_TABLE_SZ>{detail::sin_table});
+    detail::precalc<PRECALC_DEFAULT>::init_sin(estd::span<float, EMBR_DSP_PRECALC_TABLE_SZ>{table});
 }
+
+#if FEATURE_EMBR_DSP_PRECALC_TABLE_STATIC
+void init_sin_table() { init_sin_table_(detail::sin_table); }
+#else
+void init_sin_table(float* table) { init_sin_table_(table); }
+#endif
+
 #endif
 
 }}}
--- src/embr/dsp/precalc/fwd.h
+++ src/embr/dsp/precalc/fwd.h
@@ -7,7 +7,11 @@ namespace embr { namespace dsp { inline namespace v1 {
 
 namespace detail {
 
+#if FEATURE_EMBR_DSP_PRECALC_TABLE_STATIC
 extern float sin_table[EMBR_DSP_PRECALC_TABLE_SZ];
+#else
+extern float* sin_table;
+#endif
 
 template <precalc_modes>
 struct precalc;
