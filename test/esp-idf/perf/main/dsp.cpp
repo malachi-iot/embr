215d74ef3cda3bf760f50bcbb0dee4bacb999c5e
Malachi Burke <malachi.burke@gmail.com>
2025-03-10 12:48:47 -0700

    feat: boilerplate

--- test/esp-idf/perf/main/dsp.cpp
+++ test/esp-idf/perf/main/dsp.cpp
@@ -8,5 +8,19 @@ static const char* TAG = "embr::perf::dsp";
 
 void test_dsp()
 {
+    embr::dsp::init_sin_table();
 
+    for(float v = 0; v < 1; v += 0.001)
+    {
+
+    }
+
+    ESP_LOGI(TAG, "");
+
+    for(float v = 0; v < 1; v += 0.001)
+    {
+
+    }
+
+    ESP_LOGI(TAG, "");
 }
