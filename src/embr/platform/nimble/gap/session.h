3370c2abf9f3ff428b1bc2c7d819355ca4989ab0
Malachi Burke <malachi.burke@gmail.com>
2025-04-06 20:08:13 -0700

    refactor!: changing ble_addr_type -> addr_type

--- src/embr/platform/nimble/gap/session.h
+++ src/embr/platform/nimble/gap/session.h
@@ -9,7 +9,7 @@ namespace embr { namespace nimble { namespace gap { inline namespace v1 {
 
 struct Session
 {
-    uint8_t ble_addr_type;
+    uint8_t addr_type;
     uint16_t conn_handle;
 
     // Unsure what formal values apply to "reason code", if any
@@ -21,6 +21,17 @@ struct Session
     int gap_conn_find(ble_gap_conn_desc* desc) const
     {
         return ble_gap_conn_find(conn_handle, desc);
+    }
+
+    int gap_adv_start(const ble_addr_t* direct_addr, int32_t duration_ms,
+        const ble_gap_adv_params* adv_params, ble_gap_event_fn* cb, void* cb_arg = nullptr)
+    {
+        return ble_gap_adv_start(addr_type, direct_addr, duration_ms, adv_params, cb, cb_arg);
+    }
+
+    int gap_adv_start(const ble_gap_adv_params* adv_params, ble_gap_event_fn* cb, void* cb_arg = nullptr)
+    {
+        return ble_gap_adv_start(addr_type, nullptr, BLE_HS_FOREVER, adv_params, cb, cb_arg);
     }
 };
 
