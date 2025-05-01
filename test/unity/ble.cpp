76ab3050d693ae070d16c4768db20c25fcf5b382
Malachi Burke <malachi.burke@gmail.com>
2025-05-01 16:29:29 -0700

    test: very minor additional testing

--- test/unity/ble.cpp
+++ test/unity/ble.cpp
@@ -6,15 +6,31 @@ using namespace embr::ble;
 
 // No physical/transport things here, just data structures
 
-static void test_ots()
-{
-    using oacp_type = gatt::v1::ObjectActionControlPoint;
+using oacp_type = gatt::v1::ObjectActionControlPoint;
 
+
+static void test_ots_oacp_checksum()
+{
     oacp_type oacp;
 
     oacp.opcode = oacp_type::CHECKSUM;
     oacp.checksum.length = 10;
     oacp.checksum.offset = 10;
+}
+
+static void test_ots_oacp_response()
+{
+    oacp_type oacp
+    {
+        .opcode = oacp_type::RESPONSE_CODE,
+        .response
+        {
+            .opcode = oacp_type::CREATE,
+            .result_code = oacp_type::SUCCESS,
+            // DEBT: Find a way to squish the need for parameter{}
+            .parameter {}
+        }
+    };
 }
 
 #ifdef ESP_IDF_TESTING
@@ -23,5 +39,6 @@ TEST_CASE("ble tests", "[ble]")
 void test_ble()
 #endif
 {
-    RUN_TEST(test_ots);
+    RUN_TEST(test_ots_oacp_checksum);
+    RUN_TEST(test_ots_oacp_response);
 }
