a9bc89bafa6305b982a465cce9184377b7ffe125
Malachi Burke <malachi.burke@gmail.com>
2025-05-04 09:19:17 -0700

    feat(nvs): bits and pieces

--- lib/esp-idf/embr-storage/include/embr/esp-idf/nvs-allocator.h
+++ lib/esp-idf/embr-storage/include/embr/esp-idf/nvs-allocator.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <esp_partition.h>
-#include <wear_levelling.h>
+//#include <wear_levelling.h>
 
 namespace embr::esp_idf {
 
@@ -10,7 +10,30 @@ namespace embr::esp_idf {
 
 struct nvs_allocator_base
 {
+    struct header
+    {
+        uint32_t next;
+        uint32_t size;
+    };
 
+    const esp_partition_t* partition_;
+    //wl_handle_t* wl_handle_;
+
+    void open()
+    {
+        partition_ = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_ANY, "embr");
+        //ESP_ERROR_CHECK(wl_mount(partition_, wl_handle_));
+    }
+
+    esp_err_t mmap(const void** data, esp_partition_mmap_handle_t* out_handle)
+    {
+        return esp_partition_mmap(partition_, 0, 4096, ESP_PARTITION_MMAP_DATA, data, out_handle);
+    }
+
+    void write(size_t addr, const void* src, size_t size)
+    {
+
+    }
 };
 
 }
