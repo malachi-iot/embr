e2b7a9f930e260a30774f16e759cca24f0ec9111
Malachi Burke <malachi.burke@gmail.com>
2025-05-05 11:02:38 -0700

    feat: emergency checkin

--- lib/esp-idf/embr-storage/include/embr/esp-idf/nvs-allocator.h
+++ lib/esp-idf/embr-storage/include/embr/esp-idf/nvs-allocator.h
@@ -1,8 +1,13 @@
 #pragma once
 
+//#include <freertos/FreeRTOS.h>
+//#include <freertos/semphr.h>
+
 #include <spi_flash_mmap.h>
 #include <esp_partition.h>
 //#include <wear_levelling.h>
+
+#include <estd/port/freertos/mutex.h>
 
 namespace embr::esp_idf {
 
@@ -18,25 +23,52 @@ struct nvs_allocator_base
     using mmap_handle_t = esp_partition_mmap_handle_t;
 
     static constexpr unsigned sector_size = SPI_FLASH_SEC_SIZE;
+    static constexpr unsigned block_size = 256;
     static constexpr auto npos = (uint16_t) -1;
 
-    // Positions/sizes are in multiples of sector_size
+    static_assert(block_size < sector_size && sector_size % block_size == 0);
 
-    // DEBT: Can be optimized down
+    /*
+    struct subheader
+    {
+        uint16_t size_in_blocks;
+
+    }   __attribute__((packed));    */
+
+
     struct header
     {
-        uint16_t size_in_sectors;
-        uint16_t id;    // 0 = control table
+        uint16_t size_in_blocks;
+        uint16_t id : 14;    // 0 = control table
         uint16_t allocated : 1;
 
         uint16_t size_in_bytes() const
         {
-            return size_in_sectors * sector_size;
+            return size_in_blocks * block_size;
         }
+
+        uint16_t size_in_sectors() const
+        {
+            return size_in_blocks * (sector_size / block_size);
+        }
+
+        // array count = size_in_sectors
+        uint8_t write_counter[];
+
+        // DEBT: Perhaps add an aligned flag and align to 4 byte boundary
+        void* payload()
+        {
+            return this + (offsetof(header, write_counter) + size_in_sectors());
+        }
+
     }   __attribute__((packed));
 
     const esp_partition_t* partition_;
+    const void* data_ {};
     //wl_handle_t* wl_handle_;
+    mmap_handle_t mmap_handle_;
+    estd::freertos::timed_mutex<true> data_lock_;
+    //SemaphoreHandle_t data_lock_;
 
     void open()
     {
@@ -48,6 +80,23 @@ struct nvs_allocator_base
     {
         return esp_partition_mmap(partition_, offset, sector_size, ESP_PARTITION_MMAP_DATA, data, out_handle);
     }
+
+    // NOTE: offset isn't truly needed and mainly used for debugging who locked mmap
+    esp_err_t mmap(size_t offset)
+    {
+        if(!data_lock_.try_lock_for(estd::chrono::seconds(1)))
+            return ESP_ERR_TIMEOUT;
+        return mmap(offset, &data_, &mmap_handle_);
+    }
+
+    void munmap()
+    {
+        esp_partition_munmap(mmap_handle_);
+        data_ = nullptr;
+        data_lock_.unlock();
+    }
+
+    const header* data() const { return data_; } 
 
     // DEBT: We're gonna find out soon why they keep these APIs separate I'm sure
     esp_err_t write(size_t addr, const void* src, size_t size)
@@ -64,26 +113,30 @@ struct nvs_allocator_base
     struct accessor
     {
         nvs_allocator_base* parent_;
-        uint16_t pos_in_sectors_;  // x sector size
+        uint16_t pos_in_blocks_;
         // DEBT: Side effect-y, for now
-        mmap_handle_t mmap_handle_;
-        uint16_t pos() const { return pos_in_sectors_ * sector_size; }
+        //mmap_handle_t mmap_handle_;
+        uint16_t pos() const { return pos_in_blocks_ * block_size; }
 
         const header* mmap()
         {
-            const void* data;
-            ESP_ERROR_CHECK(parent_->mmap(pos(), &data, &mmap_handle_));
-            return (const header*) data;
+            //const void* data;
+            //ESP_ERROR_CHECK(parent_->mmap(pos(), &data, &mmap_handle_));
+            ESP_ERROR_CHECK(parent_->mmap(pos()));
+            //auto data = (const uint8_t*) parent_->data_;
+            //return (const header*) (data + pos());
+            return parent_->data();
         }
 
         void munmap()
         {
-            esp_partition_munmap(mmap_handle_);
+            //esp_partition_munmap(mmap_handle_);
+            parent_->munmap();
         }
 
         constexpr bool operator ==(const accessor& other) const
         {
-            return pos_in_sectors_ == other.pos_in_sectors_;
+            return pos_in_blocks_ == other.pos_in_blocks_;
         }
     };
 
@@ -93,7 +146,7 @@ struct nvs_allocator_base
     {
         iterator& operator++()
         {
-            pos_in_sectors_ += mmap()->size_in_sectors;
+            pos_in_blocks_ += mmap()->size_in_blocks;
             //pos_ = mmap()->next;
             munmap();
             return *this;
@@ -107,7 +160,7 @@ struct nvs_allocator_base
         iterator it;
 
         it.parent_ = this;
-        it.pos_in_sectors_ = 0;
+        it.pos_in_blocks_ = 0;
 
         return it;
     }
@@ -118,7 +171,7 @@ struct nvs_allocator_base
         iterator it;
 
         it.parent_ = this;
-        it.pos_in_sectors_ = npos;
+        it.pos_in_blocks_ = npos;
 
         return it;
     }
