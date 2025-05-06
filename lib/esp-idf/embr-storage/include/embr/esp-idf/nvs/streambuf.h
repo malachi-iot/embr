d365bba3351584213df3ba76d2a4de777fc5d8b8
Malachi Burke <malachi.burke@gmail.com>
2025-05-06 08:41:06 -0700

    feat(nvs): slowly fleshing out streambuf
    Checking in errors, battery dead

--- lib/esp-idf/embr-storage/include/embr/esp-idf/nvs/streambuf.h
+++ lib/esp-idf/embr-storage/include/embr/esp-idf/nvs/streambuf.h
@@ -24,9 +24,15 @@ class ostreambuf :
     void sync()
     {
         const unsigned offset = offset_ * sector_size;
-        esp_partition_erase_range(partition_, offset, sector_size);
+        esp_err_t ret;
+        ret = esp_partition_erase_range(partition_, offset, sector_size);
+        if(ret != ESP_OK) return -1;
         esp_partition_write(partition_, offset, base_type::pbase(), sector_size);
+        return ret == ESP_OK ? 0 : -1;
     }
+
+    // amount of buffer space left we can write to
+    constexpr int_type xout_avail() const { return sector_size - this->pos(); }
 
 protected:
     int_type overflow(int_type ch = traits_type::eof())
