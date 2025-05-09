c32eba15213aa46497c5c8cbad4c444c5b866425
Malachi Burke <malachi.burke@gmail.com>
2025-05-09 09:20:58 -0700

    build: end of an era, goodbye estd submodule
    Tagging as v0.4.0 since it's a big step, even if not technically a breaking change

--- src/embr/version.h
+++ src/embr/version.h
@@ -3,8 +3,8 @@
 #include <estd/port/arch/version.h>
 
 #define EMBR_VER_MAJOR     0
-#define EMBR_VER_MINOR     3
-#define EMBR_VER_PATCH     2
+#define EMBR_VER_MINOR     4
+#define EMBR_VER_PATCH     0
 
-#define EMBR_VERSION  ESTD_BUILD_SEMVER(0, 3, 2)
-#define EMBR_VERSION_STR "0.3.2"
+#define EMBR_VERSION  ESTD_BUILD_SEMVER(0, 4, 0)
+#define EMBR_VERSION_STR "0.4.0"
