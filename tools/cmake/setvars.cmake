7ad7c8e7fcbca8c3381bbf39a9eb05ace3a918ef
Malachi Burke <malachi.burke@gmail.com>
2025-05-26 08:39:14 -0700

    build: dogfooding in top-level CMakeLists

--- tools/cmake/setvars.cmake
+++ tools/cmake/setvars.cmake
@@ -2,5 +2,6 @@ include_guard(GLOBAL)
 
 get_filename_component(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 set(EMBR_ROOT_DIR ${ROOT_DIR} PARENT_SCOPE)
+#set(EMBR_ROOT_DIR ${ROOT_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
