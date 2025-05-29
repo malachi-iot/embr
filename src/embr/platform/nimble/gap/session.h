d07c740151355cbae0766e8cfe7bebf88c26372d
Malachi Burke <malachi.burke@gmail.com>
2025-05-29 10:25:31 -0700

    feat(ble): refining gap_terminate wrapper + adding params + security API

--- src/embr/platform/nimble/gap/session.h
+++ src/embr/platform/nimble/gap/session.h
@@ -14,15 +14,20 @@ struct Session
     uint8_t addr_type;
     uint16_t conn_handle;
 
-    // Unsure what formal values apply to "reason code", if any
-    int gap_terminate(uint8_t hci_reason = 0) const
+    // See nimble/ble.h ble_error_codes enum
+    int gap_terminate(uint8_t hci_reason = BLE_ERR_SUCCESS) const
     {
-        return ble_gap_terminate(conn_handle, 0);
+        return ble_gap_terminate(conn_handle, hci_reason);
     }
 
     int gap_conn_find(ble_gap_conn_desc* desc) const
     {
         return ble_gap_conn_find(conn_handle, desc);
+    }
+
+    int gap_update_params(const ble_gap_upd_params* params)
+    {
+        return ble_gap_update_params(conn_handle, params);
     }
 
     int gap_adv_start(const ble_addr_t* direct_addr, int32_t duration_ms,
@@ -34,6 +39,11 @@ struct Session
     int gap_adv_start(const ble_gap_adv_params* adv_params, ble_gap_event_fn* cb, void* cb_arg = nullptr)
     {
         return ble_gap_adv_start(addr_type, nullptr, BLE_HS_FOREVER, adv_params, cb, cb_arg);
+    }
+
+    int gap_security_initiate() const
+    {
+        return ble_gap_security_initiate(conn_handle);
     }
 };
 
