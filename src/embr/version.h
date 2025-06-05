eb40919bc6a8d32f46e3a179354c769742f905f9
Malachi Burke <malachi.burke@gmail.com>
2025-06-05 08:16:18 -0700

    Release v0.4.1

--- src/embr/version.h
+++ src/embr/version.h
@@ -4,7 +4,7 @@
 
 #define EMBR_VER_MAJOR     0
 #define EMBR_VER_MINOR     4
-#define EMBR_VER_PATCH     0
+#define EMBR_VER_PATCH     1
 
-#define EMBR_VERSION  ESTD_BUILD_SEMVER(0, 4, 0)
-#define EMBR_VERSION_STR "0.4.0"
+#define EMBR_VERSION  ESTD_BUILD_SEMVER(0, 4, 1)
+#define EMBR_VERSION_STR "0.4.1"
