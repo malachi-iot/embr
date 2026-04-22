68799a6a1405a66cf88972cedbbbf8749896d1b7
Malachi Burke <malachi.burke@gmail.com>
2026-04-22 09:10:42 -0700

    build: top-level cleanup + adding clang GH workflow

--- tools/cmake/setvars.cmake
+++ tools/cmake/setvars.cmake
@@ -1,7 +1,7 @@
 include_guard(GLOBAL)
 
 get_filename_component(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
-set(EMBR_ROOT_DIR ${ROOT_DIR} PARENT_SCOPE)
+set(EMBR_ROOT_DIR ${ROOT_DIR} CACHE STRING "Absolute path to embr")
 #set(EMBR_ROOT_DIR ${ROOT_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
