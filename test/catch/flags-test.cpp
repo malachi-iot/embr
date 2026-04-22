e9a16069e963e14976c658ca69e434b29d4e320d
Malachi Burke <malachi.burke@gmail.com>
2026-04-22 09:55:09 -0700

    test: clang testing
    Noticing a glitch in estd, bool operator not enabled for 'flags' with clang

--- test/catch/flags-test.cpp
+++ test/catch/flags-test.cpp
@@ -14,15 +14,15 @@ TEST_CASE("flags (compile-time capable)", "[flags]")
         auto v = ~v2::word_options::native;
         //auto v2 = unsigned(v.value());
 
-        REQUIRE((v & v2::word_options::native) == false);
-        REQUIRE((v & v2::word_options::implicit) == true);
+        REQUIRE(is_set(v & v2::word_options::native) == false);
+        REQUIRE(is_set(v & v2::word_options::implicit) == true);
     }
     SECTION("xor")
     {
         constexpr auto v1 = v2::word_options::implicit;
         constexpr auto v2 = v1 ^ v2::word_options::implicit;
 
-        REQUIRE(v2 == false);
+        static_assert(v2 == false, "xor failed");
     }
     SECTION("compare")
     {
