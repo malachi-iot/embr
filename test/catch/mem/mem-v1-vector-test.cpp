14c0424d3c441d79b58d8237e9ca6c06458cb327
Malachi Burke <malachi.burke@gmail.com>
2026-05-05 18:42:26 -0700

    build: estd allocator/array cleanup enough now for embr::gc vector to build again

--- test/catch/mem/mem-v1-vector-test.cpp
+++ test/catch/mem/mem-v1-vector-test.cpp
@@ -286,8 +286,6 @@ TEST_CASE("gc mem v1 vector", "[memory][gc][vector]")
         REQUIRE(bn2.handle != bn.handle);
         REQUIRE(bn.handle == 2);
 
-        // FIX: Somehow this guy isn't selecting dynamic_array copy/move constructor and going straight
-        // for perfect-forwarder
         vector_type copied(vector);
         const auto& copied_revealed = (vector_revealed<int, pool_type>&)copied;
 
