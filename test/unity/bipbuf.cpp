e38dc159f7a4a8f4a865ca985810054d051334e0
Malachi Burke <malachi.burke@gmail.com>
2026-05-24 11:42:20 -0700

    test: fully async test, prep for better fail resiliency

--- test/unity/bipbuf.cpp
+++ test/unity/bipbuf.cpp
@@ -77,50 +77,75 @@ struct test_mutex
     void unlock() { mutex_.unlock(); }
 };
 
-template <class Buf>
-static void test_async(bipbuf<Buf>& mbb, std::mt19937& gen)
+template <class Bipbuf>
+struct shared_type
+{
+    using message = typename Bipbuf::message;
+
+    rtos::task parent;
+    Bipbuf& mbb;
+    std::mt19937& gen;
+    test_mutex mutex;
+
+    constexpr static int loops = 10;
+
+    void push()
+    {
+        const uint8_t sz = gen() % 32;
+
+        [[maybe_unused]]
+        estd::errc err = mbb.push(mutex, [sz](message* m)
+        {
+            estd::fill_n((char*)m->payload(), sz, sz);
+        }, sz);
+    }
+
+    void do_things()
+    {
+        for(int i = 0; i < loops; ++i) push();
+
+        parent.notify_give(0);
+    }
+};
+
+using layer1_type = bipbuf<estd::layer1::bipbuf<128>>;
+
+// TODO: Not used yet, this is theoretically a way to overcome part fault
+// on unit test failure.  In particular, worker tasks are still running but
+// shared state goes out of scope.  I don't want to allocate it globally,
+// so the idea is if we allocate it on the stack before RUN_TEST occurs,
+// that might do the trick.
+[[maybe_unused]]
+static union
+{
+    shared_type<layer1_type>* layer1;
+}   shared;
+
+static constexpr int task_count = 3;
+
+static void wait_for_worker_finish()
+{
+    for(int i = 0; i < task_count; ++i)
+        ulTaskNotifyTakeIndexed(0, pdFALSE, portMAX_DELAY);
+}
+
+
+template <class Bipbuf, class Buf>
+static void test_async(shared_type<Bipbuf>& shared, bipbuf<Buf>& mbb, std::mt19937& gen)
 {
     using message = typename bipbuf<Buf>::message;
-
-    struct shared_type
-    {
-        rtos::task parent;
-        bipbuf<Buf>& mbb;
-        std::mt19937& gen;
-        test_mutex mutex;
-
-        void push()
-        {
-            const uint8_t sz = gen() % 32;
-
-            [[maybe_unused]]
-            estd::errc err = mbb.push(mutex, [sz](message* m)
-            {
-                estd::fill_n((char*)m->payload(), sz, sz);
-            }, sz);
-        }
-
-        void do_things()
-        {
-            for(int i = 0; i < 10; ++i) push();
-
-            parent.notify_give(0);
-        }
-
-        // DEBT: Why does out mutex buddy need this explicit initializer?  Odd.
-    }   shared{rtos::task::current(), mbb, gen, {}};
 
     auto f = [](void* arg)
     {
         // DO NOT LOG HERE! 2K stack isn't enough for that
-        auto shared = (shared_type*) arg;
+        auto shared = (shared_type<Bipbuf>*) arg;
 
         shared->do_things();
 
         vTaskDelete(nullptr);
     };
 
-    rtos::task tasks[2];
+    rtos::task tasks[task_count];
     for(rtos::task& task : tasks)
     {
         BaseType_t r = task.create(f, "bipbuf worker", 2048, &shared, 1);
@@ -129,9 +154,9 @@ static void test_async(bipbuf<Buf>& mbb, std::mt19937& gen)
 
     int counter = 0;
 
-    for(int i = 0; i < 20 && counter < 1000;++counter)
+    for(int i = 0; i < task_count * shared.loops && counter < 1000;++counter)
     {
-        estd::errc err = mbb.pop(shared.mutex, [&](message* p)
+        estd::errc err = mbb.pop(shared.mutex, [&](const message* p)
         {
             char temp[32];
             uint8_t sz = p->payload_size();
@@ -147,9 +172,7 @@ static void test_async(bipbuf<Buf>& mbb, std::mt19937& gen)
         }
     }
 
-    // Wait for workers to finish
-    for(auto& _ : tasks)
-        ulTaskNotifyTakeIndexed(0, pdFALSE, portMAX_DELAY);
+    wait_for_worker_finish();
 
     TEST_ASSERT_LESS_THAN(10000, counter);
 }
@@ -161,15 +184,19 @@ static void test_layer1()
 {
     // DEBT: Whoa!  mt19937 uses over 2k of stack!!  Find an alternative
     static std::mt19937 gen{}; // NOLINT fixed seed: deterministic sequence = what we want
-    using type = bipbuf<estd::layer1::bipbuf<128>>;
-    
+    using type = layer1_type;
     {
         type mbb;
         test_list(mbb, gen);
     }
     {
         type mbb;
-        test_async(mbb, gen);
+        shared_type<layer1_type>
+        // DEBT: Why does out mutex buddy need this explicit initializer?  Odd.
+        shared{rtos::task::current(), mbb, gen, {}};
+        ::shared.layer1 = &shared;
+        
+        test_async(*::shared.layer1, mbb, gen);
     }
 }
 
@@ -179,5 +206,7 @@ TEST_CASE("bipbuf", "[bipbuf]")
 void test_bipbuf()
 #endif
 {
-    RUN_TEST(test_layer1);
+    {
+        RUN_TEST(test_layer1);
+    }
 }
